21bdb8d6ce9bdf81a3c0072433632ceede7f1c10
ansumariamjacob <ansumariam99@gmail.com>
2022-05-17 09:20:38 +0530

    searchStudent

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import AddStudent from './component/AddStudent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import SearchStudent from './component/SearchStudent';
 
 function App() {
   return (
     <div >
-      <AddStudent/>
+      <SearchStudent/>
     </div>
   );
 }
